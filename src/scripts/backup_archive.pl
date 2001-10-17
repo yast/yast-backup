d5f9b847cd1aa6f36635ec4639e9ce59e64298d1
Ladislav Slezák <lslezak@suse.cz>
2001-10-17 12:24:50 +0000

    initial revision
    svn path=/trunk/backup/; revision=1764

---
+++ src/scripts/backup_archive.pl
@@ -0,0 +1,366 @@
+#!/usr/bin/perl
+#
+#  File:
+#    backup_archive.pl
+#
+#  Module:
+#    Backup module
+#
+#  Authors:
+#    Ladislav Slezak <lslezak@suse.cz>
+#
+#  Description:
+#    This script creates backup archive as scpecified with command
+#    line parameters.
+#
+# $Id$
+#
+
+use Getopt::Long;
+#TODO use strict;
+
+#use File::Temp qw/ tempfile tempdir /;  # not in perl 5.6.0
+ 
+use POSIX qw(tmpnam strftime);
+
+# command line options
+my $archive_name = '';
+my $archive_type = '';
+my $help = '0';
+my $store_pt = '0';
+my @ext2_parts = ();
+my $verbose = '0';
+my $files_info = '';
+my $comment_file = '';
+
+# parse command line options
+GetOptions('archive-name=s' => \$archive_name, 
+    'archive-type=s' => \$archive_type, 'help' => \$help,
+    'store-ptable' => \$store_pt, 'store-ext2=s' => \@ext2_parts,
+    'verbose' => \$verbose, 'files-info=s' => \$files_info,
+    'comment-file=s'=> \$comment_file
+);
+
+
+if ($help or $files_info eq '' or $archive_name eq '')
+{
+    print "Usage: $0 [options]\n\n";
+    
+    print "This script creates backup archive as specified in input file and in command line options.\n";
+    print "Options --archive-name and --files-info are mandatory.\n\n";
+    
+    print "Options:\n\n";
+    
+    print "  --help                Display this help\n\n";
+    print "  --archive-name <file> Target archive file name\n";
+    print "  --archive-type <type> Type of compression used by tar, type can be 'tgz' - compressed by gzip, 'tbz2' - compressed by bzip2 or 'tar' - no compression, default is 'tgz'\n";
+    print "  --verbose             Print progress information\n";
+    print "  --store-ptable        Add partition table information to archive\n";
+    print "  --store-ext2 <device> Store Ext2 system area from device\n";
+    print "  --files-info          Data file from backup_search script\n";
+    print "  --comment-file <file> Use comment stored in file\n\n";
+        
+    exit 0;
+}
+
+
+$| = 1;
+
+# archive type option
+if ($archive_type ne 'tgz' && $archive_type ne 'tbz2' && $archive_type ne 'tar')
+{
+    $archive_type = 'tgz';
+}
+
+# for security reasons set permissions only to owner
+umask(0077);
+
+my $tmp_dir_root = tmpnam();
+
+do 
+{
+    while (-e $tmp_dir_root)
+    {
+	$tmp_dir_root = tmpnam();
+    }
+}
+until mkdir($tmp_dir_root, 0700);
+
+
+my $tmp_dir = $tmp_dir_root."/tmp";
+mkdir($tmp_dir);
+
+$tmp_dir .= "/YaST2-backup";
+mkdir($tmp_dir);
+
+
+my $files_num = 0;
+
+open(OUT, '>', $tmp_dir_root."/files")
+    or die "Can not open file $tmp_dir_root/files\n";
+
+
+# store host name
+use Sys::Hostname;
+my $host = hostname();
+
+if ($verbose)
+{
+    print "Storing hostname: ";
+}
+
+open(HOST, '>', $tmp_dir.'/hostname');
+print HOST $host;
+close(HOST);
+
+if (-s $tmp_dir.'/hostname')
+{
+    if ($verbose)
+    {
+	print "Success\n";
+    }
+    
+    print OUT "tmp/YaST2-backup/hostname\n";
+    $files_num++;
+}
+else
+{
+    if ($verbose)
+    {
+	print "Failed\n";
+    }
+}
+
+
+# store date
+if ($verbose)
+{
+    print "Storing date: ";
+}
+
+my $date = strftime('%x %X', localtime());
+
+open(DATE, '>', $tmp_dir.'/date');
+print DATE $date;
+close(DATE);
+
+
+if (-s $tmp_dir.'/date')
+{
+    if ($verbose)
+    {
+	print "Success\n";
+    }
+    
+    print OUT "tmp/YaST2-backup/date\n";
+    $files_num++;
+}
+else
+{
+    if ($verbose)
+    {
+	print "Failed\n";
+    }
+}
+
+# store partition table info
+if ($store_pt)
+{
+    if ($verbose)
+    {
+	print "Storing partition table: ";
+    }
+    
+    system('/sbin/fdisk -l > '.$tmp_dir.'/partition_table 2> /dev/null');
+
+    if (-s  $tmp_dir.'/partition_table')
+    {
+	if ($verbose)
+	{
+	    print "Success\n";
+	}
+	
+	print OUT "tmp/YaST2-backup/partition_table\n";
+	$files_num++;
+    }
+    else
+    {	
+	if ($verbose)
+	{
+	    print "Failed\n";
+	}
+    }
+}
+
+
+# filter files_info file, output only file names
+
+open(FILES_INFO, "> $tmp_dir/files_info")
+    or die "Can not create file $tmp_dir/files_info\n";
+
+if (defined open(FILES, $files_info))
+{
+    while (my $line = <FILES>)
+    {
+	chomp($line);
+	
+	if ($line =~ /\/.+/)
+	{
+	    if (-r $line)		# output only readable files from files-info
+	    {
+		print OUT $line."\n";
+		$files_num++;
+		print FILES_INFO $line."\n";
+	    }
+	    else
+	    {
+		print STDERR "File $line is not readable.\n"
+	    }
+	}
+	else
+	{
+	    print FILES_INFO $line."\n";
+	}
+    }
+    
+    close(FILES);
+}
+
+close(FILES_INFO);
+
+
+if (length($comment_file) > 0)
+{
+    system("cp $comment_file $tmp_dir/comment");
+
+    if ($? == 0)
+    {
+	print OUT "tmp/YaST2-backup/comment\n";
+	$files_num++;
+    }
+}
+
+    
+if ($stored_pt)
+{
+    print OUT "tmp/YaST2-backup/partition_table\n";
+    $files_num++;
+}
+
+# TODO files info should be first file for faster unpacking (???)
+# ... seems NO, tar does not exit when selected file is unpacked
+# note: tar writes file name after starting unpacking of this file
+
+print OUT "tmp/YaST2-backup/files_info\n";
+$files_num++;
+
+
+#store ext2 system area
+
+foreach my $part (@ext2_parts)
+{
+    if ($verbose)
+    {
+	print 'Storing ext2 area: '.$part."\n";
+    }
+
+    # transliterate all '/' characters to '_' in device name
+    my $tr_dev_name = $part;
+    $tr_dev_name =~ tr/\//_/;
+
+    my $output_name = $tmp_dir."/e2image".$tr_dev_name;
+
+    system("/sbin/e2image $part $output_name");
+
+    if (-s $output_name)
+    {
+	if ($verbose)
+	{
+	    print "Success\n";
+	    print OUT "/tmp/YaST2-backup/e2image_".$tr_dev_name;
+	    $files_num++;
+	}
+    }
+    else
+    {
+	if ($verbose)
+	{
+	    print "Failed\n";
+	}
+    }
+}
+
+
+close(OUT);
+
+
+if ($verbose)
+{
+    print "Files: $files_num\n";
+}
+
+
+# used tar options:
+#  -c 			create archive
+#  -f <file> 		archive file name
+#  --files-from <file>	read list from file
+#  -z			pack archive by gzip
+#  -j			pack archive by bzip2
+#  -v			verbose output
+#  --ignore-failed-read	continue after read error
+#  -C <dir>		change to dir befor archiving
+#  -S			store sparse files efficiently (for e2images)
+
+my $tar_command = "tar -c -f $archive_name --files-from $tmp_dir_root/files --ignore-failed-read -C $tmp_dir_root -S";
+
+if ($verbose)
+{
+    $tar_command .= ' -v';
+}
+
+if ($archive_type eq 'tgz')
+{
+    $tar_command .= ' -z';
+}
+else
+{
+    if ($archive_type eq 'tbz2')
+    {
+	$tar_command .= ' -j';
+    }
+}
+
+
+system($tar_command.' 2> /dev/null');
+
+if ($verbose)
+{
+    print "/Tar result: $?\n";
+}
+
+
+# delete contents of temporary directory
+# TODO: better cleanup - unlink only created files, no blind unlink of possibly created files...
+
+unlink($tmp_dir.'/hostname');
+unlink($tmp_dir.'/date');
+unlink($tmp_dir.'/comment');
+unlink($tmp_dir.'/partition_table');
+unlink($tmp_dir.'/files_info');
+unlink($tmp_dir_root.'/files');
+
+
+foreach my $part (@ext2_parts)
+{
+    # transliterate all '/' characters to '_' in device name
+    my $tr_dev_name = $part;
+    $tr_dev_name =~ tr/\//_/;
+
+    unlink($tmp_dir.'/e2image'.$tr_dev_name);
+}
+
+rmdir($tmp_dir);
+rmdir($tmp_dir_root.'/tmp');
+rmdir($tmp_dir_root);
+
+
---
+++ src/scripts/backup_search.pl
@@ -0,0 +1,416 @@
+#!/usr/bin/perl
+#
+#  File:
+#    backup_files.pl
+#
+#  Module:
+#    Backup module
+#
+#  Authors:
+#    Ladislav Slezak <lslezak@suse.cz>
+#
+#  Description:
+#    This script searches modified files in system (comparing
+#    to the RPM database) a and files which do not belong do any
+#    package. Output is list of files and optional more
+#    information about progress of search.
+#
+# $Id$
+#
+
+use Getopt::Long;
+use strict;
+
+# command line options
+my $search_files = 0;
+my @exclude_d = ();
+my @exclude_fs = ();
+my $help = 0;
+
+my $output_progress = 0;
+my $output_files = 0;
+my $output_default = 0;
+my $enhanced_check_nopkg = 0;
+my $disable_check_multiple = 0;
+
+my %exclude_dirs;
+
+# parse command line options
+GetOptions('search' => \$search_files, 'exclude-dir=s' => \@exclude_d,
+    'exclude-fs=s' => \@exclude_fs,  'help' => \$help,
+    'output-progress' => \$output_progress, 'output-files' => \$output_files,
+    'output-default' => \$output_default, 'enhanced-check' => \$enhanced_check_nopkg,
+    'disable-check-multiple' => \$disable_check_multiple 
+);
+
+if ($help)
+{
+    print "Usage: $0 [options]\n";
+    print "\nSearch modified files in packages to backup, optionaly search files which\n";
+    print "  do not belong to any package.\n\n";
+
+    print "Options:\n\n";
+
+
+    print "  --disable-check-multiple   Do not check if backup is needed for changed files \
+which are in multiple packages, always backup them\n\n";
+
+    print "  --search           Search files which do not belog to any package\n";
+    print "    --exclude-dir <dir>  Exclude directory <dir> from search\n";
+    print "    --exclude-fs <fs>    Exclude filesystem <fs> from search\n";
+    print "    --enhanced-check     Enhanced check of owner - slower search, but more accurate\n\n";
+
+    print "  --output-files     Display only names of files to backup\n";
+    print "  --output-progress  Display data for frontend\n";
+    print "  --output-default   Default output is in format accepted by 'backup_achive' script\n";
+    exit 0;
+}
+
+
+$| = 1;
+
+if (!$output_files and !$output_progress)
+{
+    $output_default = 1;
+}
+
+
+
+# convert array to hash
+foreach my $d (@exclude_d) {$exclude_dirs{$d} = 1;}
+
+# verify installed packages
+my @installed_packages = ReadAllPackages();
+
+my %packages_files;
+my %dups = ReadAllFiles(\%packages_files);
+
+VerifyPackages(\@installed_packages, \%dups);
+
+if ($search_files)
+{
+    if (!$output_files)
+    {
+	print "Nopackage:\n";
+    }
+   
+    # insert excluded mountpoints to excluded directories
+    foreach my $d (FsToDirs(@exclude_fs)) {$exclude_dirs{$d} = 1;}
+
+    # read list of all files in installed packages
+#   ReadAllFiles(\%packages_files);
+
+    # start searching from root directory
+    SearchDirectory('/', \%packages_files, \%exclude_dirs);
+}
+
+######################################################
+
+# return list of installed packages
+sub ReadAllPackages()
+{
+    # read all installed packages
+    open(RPMQA, "rpm -qa |")
+	or die "Command 'rpm -qa' failed\n";
+
+    if ($output_progress)
+    {
+	print "Reading installed packages\n";
+    }
+
+    my $line;
+    my @all_packages;
+
+    while ($line = <RPMQA>) 
+    {
+	chomp($line);
+	push(@all_packages, $line);
+    }
+
+    close(RPMQA);
+
+    if ($output_progress)
+    {
+	my $num = @all_packages;
+	print "$num packages installed\n";
+    }
+
+    return @all_packages;
+}
+
+# verify each package in the list
+sub VerifyPackages()
+{
+    my ($packages, $duplicates) = @_;
+
+    foreach my $package (@$packages) {
+	if (!$output_files)
+	{
+	    print "Package: $package\n";
+
+	    print "Installed:";
+	    system('export LC_ALL=C; rpm -q '.$package.' --queryformat " %{INSTPREFIXES}"');
+	    print "\n";
+	}
+
+	# verification of the package - do not check md5 checksum and package dependencies, do not run verify scripts
+	open(RPMV, "export LC_ALL=C; rpm -V $package --nomd5 --noscripts --nodeps |")
+	    or die "Verification of package $package failed.";
+
+	while (my $line = <RPMV>)
+	{
+	    chomp ($line);
+
+	    # modified files have set flags Size or MTime
+
+	    my $file = undef;
+
+	    my $size = ($line =~ /^S.* (\/.*)/);
+	    if ($size)
+	    {
+		$file = $1;
+	    }
+	    
+	    my $mtime = ($line =~ /^\..{6}T.* (\/.*)/);
+	    if ($mtime)
+	    {
+		$file = $1;
+	    }
+
+    	    my $backup = 1;
+	    
+	    if ($size or $mtime)
+	    {
+		# check if Mtime changed file is in more than one package
+		if (!$disable_check_multiple and $mtime and !$size and $$duplicates{$file})
+		{
+		    open(RPMQFILE, "rpm -qf $file |");
+		    my @packages_list = ();
+
+		    while (my $pkg = <RPMQFILE>)
+		    {
+			chomp($pkg);
+			
+			if ($pkg ne $package)
+			{
+			    push(@packages_list, $pkg);
+			}
+		    }
+		    close(RPMQFILE);
+
+		    foreach my $pack (@packages_list)
+		    {
+			# it is not possible to verify one file from package
+			# so all files in package are verified
+			# in this verification is not MD5 test excluded
+			# TODO LATER: don't grep and cache results of all files from package
+			open(RPMVRF, "rpm -V $pack --nodeps --noscripts | grep $file |");
+			
+			my $fl = <RPMVRF>;
+			
+			if (!defined $fl)
+			{
+			    $backup = 0;
+			}
+			else
+			{
+			    while (my $fl = <RPMVRF>)
+			    {
+				if (($fl !~ /^S.* \/./) and ($fl !~ /^\..{6}T.* \/.*/) and ($fl !~ /^..5.* \/.*/))
+				{
+				    $backup = 0;
+				}
+			    }
+			}
+			
+			close(RPMVRF);
+			
+		    }
+		}
+
+		if (defined $file and $backup)
+		{
+		    my @filestat = stat($file);
+
+		    if (!$output_files)
+		    {
+			print "Size: $filestat[7] $file\n";
+		    }
+		    else
+		    {
+			print "$file\n";
+		    }
+		}
+	    }
+	}
+
+	close(RPMV);
+    }
+}
+
+
+# read all files which belong to packages
+sub ReadAllFiles() 
+{
+    my ($all_files) = @_;
+    my %duplicates;
+    
+    open(RPMQAL, "rpm -qal |")
+	or die "Command 'rpm -qal' failed\n";
+
+    if ($output_progress)
+    {
+	print "Reading all files\n";
+    }
+
+    while (my $line = <RPMQAL>) 
+    {
+	chomp($line);
+
+	if (exists $$all_files{$line} and -f $line)
+	{
+	    $duplicates{$line} = 1;
+	}
+	else
+	{	
+	    $$all_files{$line} = 1;
+	}
+    }
+
+    close(RPMQAL);
+
+    if ($output_progress)
+    {
+     	print "Files read\n";
+    }
+
+    return %duplicates;
+}
+
+# search files which do not belong to any package
+sub SearchDirectory()
+{
+    my ($dir, $files, $exclude) = @_;
+
+    if ($output_progress)
+    {
+	print "Dir: $dir\n";
+    }
+
+    # add ending '/' if neccessary
+    if (substr($dir, length($dir) - 1) ne '/')
+    {
+	$dir .= '/';
+    }
+
+    opendir(DIR, $dir)
+	or return;
+
+    # read directory content
+    my @content = readdir(DIR);
+    closedir(DIR);
+
+    foreach my $item (@content)
+    {
+	my $fullname = $dir.$item;
+
+	if (-l $fullname)
+	{
+	    next;	# skip symbolic links
+	}
+
+	if (-f $fullname)
+	{
+	    # is file is some package?
+	    if (!$$files{$fullname})
+	    {
+		if ($enhanced_check_nopkg)
+		{
+		    # it seems that file $fullname is not any package, check this by rpm query
+		    
+		    # this double checking is needed because for example rpm -qf sax2 says that
+		    # sax2 has files in directory /var/X11R6/lib/sax, but /var/X11R6/lib is symlink
+		    # to /usr/X11R6/lib/X11, so it seems that files /usr/X11R6/lib/X11/sax/* are not
+		    # owned by any package, but rpm -qf /usr/X11R6/lib/X11/sax/* tells that files
+		    # are in package sax2
+
+		    # TODO check more files at once
+		    
+		    open(RPMQF, "rpm -qf $fullname 2> /dev/null |");
+
+		    my $line = <RPMQF>;
+
+		    if (!defined $line)	# if output is empty then file is not owned by any package
+		    {
+			if (!$output_files)
+			{
+	    		    my @filestat = stat($fullname);
+			    print "Size: $filestat[7] $fullname\n";
+			}
+			else
+			{
+			    print "$fullname\n";
+			}
+		    }
+		    else
+		    {
+			while (<RPMQF>){};	# read remaining output
+		    }
+		    
+		    close(RPMQF);
+		}
+		else
+		{
+		    if (!$output_files)
+		    {
+			my @filestat = stat($fullname);
+			print "Size: $filestat[7] $fullname\n";
+		    }
+		    else
+		    {
+			print "$fullname\n";
+		    }
+	    
+		}
+	    }
+	}
+	else
+	{
+	    # ignore . and .. directories
+	    if ($item ne "." and $item ne ".." and -d $fullname)
+	    {
+		if (!$$exclude{$fullname})
+		{
+		    SearchDirectory($fullname, $files, $exclude);
+		}
+	    }
+	}
+    }
+}
+
+# convert filesystems to mount point directories
+sub FsToDirs()
+{
+    my @fs = @_;
+    my @dirs = ();
+    my $line;
+    my @arr;
+
+    foreach my $fsys (@fs)
+    {
+	open(MOUNTT, "export LC_ALL=C; mount -t $fsys |")
+	    or next;
+     
+	while ($line = <MOUNTT>)
+	{
+	    @arr = split(/ /, $line);
+	    push(@dirs, $arr[2]);
+	}     
+
+	close(MOUNTT);
+    }
+
+    return @dirs;
+}
+
+
